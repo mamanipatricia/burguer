6fbe3601797b809ee020490ac8077bc234fa350b
patricia <patttymq@gmail.com>
2020-10-16 18:59:57 -0400

    add order modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Aux from "../../../hoc/Aux";
+
+const OrderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+    return (
+      <li key={igKey}>
+        <span className={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+        {props.ingredients[igKey]}
+      </li>
+    );
+  });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <h3>A delicious burger with the following ingredients:</h3>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to Checkout?</p>
+    </Aux>
+  );
+};
+export default OrderSummary;
