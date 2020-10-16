99b20f3c2a50fe2f00c828da96fb0857d74d9d3d
patricia <patttymq@gmail.com>
2020-10-16 17:41:31 -0400

    add 'less option' button to remove ingredients to burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -17,8 +17,11 @@ const BuildControls = (props) => (
         key={ctrl.label}
         label={ctrl.label}
         added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
       />
     ))}
   </div>
 );
+
 export default BuildControls;
