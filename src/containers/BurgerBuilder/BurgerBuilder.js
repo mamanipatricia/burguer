b28cd02850b1745469722be760ddf7337cc511d3
patricia <patttymq@gmail.com>
2020-10-16 16:40:42 -0400

    add 'more option' button to add more ingredients to burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+};
 export default class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props) {
@@ -16,14 +23,29 @@ export default class BurgerBuilder extends Component {
       cheese: 2,
       meat: 2,
     },
+    totalPrice: 4,
   };
+  addIngredientHandler = (type) => {
+    // first of all need to know what the old ingredient count is
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+  removeIngredientHandler = (type) => {};
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         {/* <div>Burger Controls</div> */}
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Aux>
     );
   }
