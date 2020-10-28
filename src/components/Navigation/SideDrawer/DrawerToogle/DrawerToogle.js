d1d1347ae85f17b8c3143e571572e94daffd14f3
patricia <patttymq@gmail.com>
2020-10-28 16:12:26 -0400

    finish toggle side drawer

--- src/components/Navigation/SideDrawer/DrawerToogle/DrawerToogle.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const drawerToogle = (props) => {
-  return <div onClick={props.clicked}>MENU</div>;
-};
-
-export default drawerToogle;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,12 @@ import React from "react";
 import classes from "./Toolbar.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-import DrawerToogle from "../SideDrawer/DrawerToogle/DrawerToogle";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     {/* <div>MENU</div> */}
-    <DrawerToogle clicked={props.drawerToggleClicked} />
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     {/* <div>LOGO</div> */}
     <div className={classes.Logo}>
       <Logo />
