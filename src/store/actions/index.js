dda498b1629c3811a631be0b95c5a55036d37928
patricia <patttymq@gmail.com>
2020-12-22 18:39:42 -0400

    Adding Actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,9 +3,12 @@ export {
   removeIngredient,
   initIngredients,
 } from "./burgerbuilder";
+
 export {
   purchaseBurgerStart,
   purchaseBurger,
   purchaseInit,
   fetchOrders,
 } from "./order";
+
+export { auth } from "./Auth";
